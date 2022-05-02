05b6dafd705550a6b87b6ccd551be1fc7b016d6d
st0bb3n <52521318+st0bb3n@users.noreply.github.com>
2022-05-02 17:35:51 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,137 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import torch
+import torchvision
+from torchvision import datasets, models
+from torchvision.transforms import functional as FT
+from torchvision import transforms as T
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, sampler, random_split, Dataset
+import copy
+from PIL import Image
+import cv2
+import albumentations as A  # our data augmentation library
+import matplotlib.pyplot as plt
+%matplotlib inline
+import datetime
+import time
+from torchvision.utils import draw_bounding_boxes
+from pycocotools.coco import COCO
+from albumentations.pytorch import ToTensorV2
+from engine import evaluate, train_one_epoch
+import utils
+
+def get_transforms(train=False):
+    if train:
+        transform = A.Compose([
+            A.Resize(416, 416),
+            ToTensorV2()
+        ], bbox_params=A.BboxParams(format='coco'))
+    else:
+        transform = A.Compose([
+            A.Resize(416, 416), 
+            ToTensorV2()
+        ], bbox_params=A.BboxParams(format='coco'))
+    return transform
+  
+class Drinks(datasets.VisionDataset):
+    def __init__(self, root, split='train', transform=None, target_transform=None, transforms=None):
+        # the 3 transform parameters are required for datasets.VisionDataset
+        super().__init__(root, transforms, transform, target_transform)
+        self.split = split #train, valid, test
+        self.coco = COCO(os.path.join(root, split, "_annotations.coco.json")) # annotations stored here
+        self.ids = list(sorted(self.coco.imgs.keys()))
+        self.ids = [id for id in self.ids if (len(self._load_target(id)) > 0)]
+    
+    def _load_image(self, id: int):
+        path = self.coco.loadImgs(id)[0]['file_name']
+        image = cv2.imread(os.path.join(self.root, self.split, path))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        return image
+
+    def _load_target(self, id):
+        return self.coco.loadAnns(self.coco.getAnnIds(id))
+    
+    def __getitem__(self, index):
+        id = self.ids[index]
+        image = self._load_image(id)
+        target = self._load_target(id)
+        target = copy.deepcopy(self._load_target(id))
+        
+        boxes = [t['bbox'] + [t['category_id']] for t in target] # required annotation format for albumentations
+        if self.transforms is not None:
+            transformed = self.transforms(image=image, bboxes=boxes)
+        
+        image = transformed['image']
+        boxes = transformed['bboxes']
+        
+        new_boxes = [] # convert from xywh to xyxy
+        for box in boxes:
+            xmin = box[0]
+            xmax = xmin + box[2]
+            ymin = box[1]
+            ymax = ymin + box[3]
+            new_boxes.append([xmin, ymin, xmax, ymax])
+        
+        boxes = torch.tensor(new_boxes, dtype=torch.float32)
+        
+        targ = {} # here is our transformed target
+        targ['boxes'] = boxes
+        targ['labels'] = torch.tensor([t['category_id'] for t in target], dtype=torch.int64)
+        #targ['image_id'] = torch.tensor([t['image_id'] for t in target])
+        targ['image_id'] = torch.tensor([index])
+        targ['area'] = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]) # we have a different area
+        targ['iscrowd'] = torch.tensor([t['iscrowd'] for t in target], dtype=torch.int64)
+        
+        return image.div(255), targ # scale images
+
+    def __len__(self):
+        return len(self.ids)
+  
+dataset_path = "/content/drive/MyDrive/drinkscoco"
+coco = COCO(os.path.join(dataset_path, "train", "_annotations.coco.json"))
+categories = coco.cats
+n_classes = len(categories.keys())
+classes = [i[1]['name'] for i in categories.items()]
+
+model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
+in_features = model.roi_heads.box_predictor.cls_score.in_features # we need to change the head
+model.roi_heads.box_predictor = models.detection.faster_rcnn.FastRCNNPredictor(in_features, n_classes)
+
+train_dataset = Drinks(root=dataset_path, transforms=get_transforms(True))
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4, collate_fn=collate_fn)
+
+images,targets = next(iter(train_loader))
+images = list(image for image in images)
+targets = [{k:v for k, v in t.items()} for t in targets]
+output = model(images, targets)
+
+device = torch.device("cuda") # use GPU to train
+model = model.to(device)
+
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.01, momentum=0.9, nesterov=True, weight_decay=1e-4)
+
+dataset = Drinks(root=dataset_path, transforms=get_transforms(True))
+test_dataset = Drinks(root=dataset_path, split="test", transforms=get_transforms(False))
+
+data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+data_loader_test = torch.utils.data.DataLoader(
+        test_dataset, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+num_epochs=10
+
+for epoch in range(num_epochs):
+    train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=50)
+    #lr_scheduler.step()
+    evaluate(model, data_loader_test, device=device)
+    
+model.eval()
+torch.cuda.empty_cache()
