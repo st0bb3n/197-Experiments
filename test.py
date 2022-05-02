b98892f57f1828d7dd02a2255d8c9ae625a38902
st0bb3n <52521318+st0bb3n@users.noreply.github.com>
2022-05-03 04:28:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -18,30 +18,15 @@ import datetime
 import time
 from torchvision.utils import draw_bounding_boxes
 from pycocotools.coco import COCO
-from albumentations.pytorch import ToTensorV2
+#from albumentations.pytorch import ToTensorV2
 from engine import evaluate, train_one_epoch
 import utils
-import wget
-import shutil
 
 url = "https://github.com/st0bb3n/ObjectDetection-Drinks/releases/download/Dataset/drinkscoco.zip"
 x = wget.download(url, "data.zip")
 
 shutil.unpack_archive("data.zip","drinkscoco")
 
-def get_transforms(train=False):
-    if train:
-        transform = A.Compose([
-            A.Resize(416, 416),
-            ToTensorV2()
-        ], bbox_params=A.BboxParams(format='coco'))
-    else:
-        transform = A.Compose([
-            A.Resize(416, 416), 
-            ToTensorV2()
-        ], bbox_params=A.BboxParams(format='coco'))
-    return transform
-  
 class Drinks(datasets.VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None, transforms=None):
         # the 3 transform parameters are required for datasets.VisionDataset
@@ -54,9 +39,9 @@ class Drinks(datasets.VisionDataset):
     def _load_image(self, id: int):
         path = self.coco.loadImgs(id)[0]['file_name']
         image = cv2.imread(os.path.join(self.root, self.split, path))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
 
-        return image
+        return image/255
 
     def _load_target(self, id):
         return self.coco.loadAnns(self.coco.getAnnIds(id))
@@ -67,12 +52,7 @@ class Drinks(datasets.VisionDataset):
         target = self._load_target(id)
         target = copy.deepcopy(self._load_target(id))
         
-        boxes = [t['bbox'] + [t['category_id']] for t in target] # required annotation format for albumentations
-        if self.transforms is not None:
-            transformed = self.transforms(image=image, bboxes=boxes)
-        
-        image = transformed['image']
-        boxes = transformed['bboxes']
+        boxes = [t['bbox'] + [t['category_id']] for t in target] 
         
         new_boxes = [] # convert from xywh to xyxy
         for box in boxes:
@@ -92,7 +72,7 @@ class Drinks(datasets.VisionDataset):
         targ['area'] = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]) # we have a different area
         targ['iscrowd'] = torch.tensor([t['iscrowd'] for t in target], dtype=torch.int64)
         
-        return image.div(255), targ # scale images
+        return torchvision.transforms.ToTensor()(image), targ # scale images
 
     def __len__(self):
         return len(self.ids)
@@ -104,32 +84,32 @@ n_classes = len(categories.keys())
 classes = [i[1]['name'] for i in categories.items()]
 
 model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
-in_features = model.roi_heads.box_predictor.cls_score.in_features # we need to change the head
+in_features = model.roi_heads.box_predictor.cls_score.in_features
 model.roi_heads.box_predictor = models.detection.faster_rcnn.FastRCNNPredictor(in_features, n_classes)
 
-train_dataset = Drinks(root=dataset_path, transforms=get_transforms(True))
-train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4, collate_fn=utils.collate_fn)
+train_dataset = Drinks(root=dataset_path, transforms=torchvision.transforms.ToTensor())
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=2, collate_fn=utils.collate_fn)
 
 images,targets = next(iter(train_loader))
 images = list(image for image in images)
 targets = [{k:v for k, v in t.items()} for t in targets]
 output = model(images, targets)
 
-device = torch.device("cuda") # use GPU to train
+device = torch.device("cuda")
 model = model.to(device)
 
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = torch.optim.SGD(params, lr=0.01, momentum=0.9, nesterov=True, weight_decay=1e-4)
 
-dataset = Drinks(root=dataset_path, transforms=get_transforms(True))
-test_dataset = Drinks(root=dataset_path, split="test", transforms=get_transforms(False))
+dataset = Drinks(root=dataset_path, transforms=torchvision.transforms.ToTensor())
+test_dataset = Drinks(root=dataset_path, split="test", transforms=torchvision.transforms.ToTensor())
 
 data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=2, shuffle=True, num_workers=4,
+        dataset, batch_size=2, shuffle=True, num_workers=2,
         collate_fn=utils.collate_fn)
 
 data_loader_test = torch.utils.data.DataLoader(
-        test_dataset, batch_size=1, shuffle=False, num_workers=4,
+        test_dataset, batch_size=1, shuffle=False, num_workers=2,
         collate_fn=utils.collate_fn)
 
 evaluate(model, data_loader_test, device=device)
