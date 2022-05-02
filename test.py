13c831cd78ccf6df3ca408a65265c6dba1f69272
st0bb3n <52521318+st0bb3n@users.noreply.github.com>
2022-05-02 17:34:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd 
 import os
 import torch
 import torchvision
